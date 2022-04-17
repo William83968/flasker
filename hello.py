b5712306bf612927c0e6a0da080a152e64f06d24
Mr.X <liuw6118@gmail.com>
2022-04-17 19:44:40 +1000

    initial commit

---
+++ hello.py
@@ -0,0 +1,32 @@
+from ssl import OP_SINGLE_DH_USE
+from flask import Flask, render_template
+
+# Create a flask instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+def index():
+    first_name = "Duchard"
+    stuff = "This is bold text"
+    cool_list = ["ice cream", 'ice block', 'milk shake', 'frozen tea', 'lemon-zero']
+    return render_template("index.html", 
+                first_name=first_name,
+                stuff = stuff,
+                cool_list=cool_list)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+
+#Create a custom error pages
+
+# Invaid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
