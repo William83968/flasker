1cce50f49b118282f26931865c9083b69e9e257b
Mr.X <liuw6118@gmail.com>
2022-04-18 17:06:09 +1000

    First Form asking for name

--- hello.py
+++ hello.py
@@ -2,9 +2,29 @@ from ssl import OP_SINGLE_DH_USE
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY']="Dastor Bilgan"
+
+# Create a Form class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Create a Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", 
+                name=name,
+                form=form)
 
 # Create a route decorator
 @app.route('/')
@@ -32,3 +52,5 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
